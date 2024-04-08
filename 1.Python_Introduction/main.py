2bf61ae904da09035e853d39c46ea3dc341a52f1
RenanM05 <r.maciel.000@gmail.com>
2024-04-08 13:29:19 +0200

    Starting the path to be a python dev

---
+++ 1.Python_Introduction/main.py
@@ -0,0 +1,9 @@
+# Python version: Python3.x
+# Print and input usage
+
+name = str(input("What is your name? "))
+age = int(input("What is your age? "))
+print("*")
+print("Your name is: ", name.upper())
+print("Your age is: ", age)
+print("*")
