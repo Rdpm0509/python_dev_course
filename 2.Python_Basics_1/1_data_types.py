fb006a87dbaf9c1050d14583fc800c6b7194f4a1
RenanM05 <r.maciel.000@gmail.com>
2024-04-08 13:56:57 +0200

    intro to fundamental math operations

---
+++ 2.Python_Basics_1/1_data_types.py
@@ -0,0 +1,24 @@
+# Data Types: 
+# Data Type is a value in python. 
+# int represents all integer numbers
+# str represents all letters 
+
+# Fundamental data types, or core to the Language
+# create, store, read, change, remove  
+
+int
+float 
+bool 
+str 
+list 
+tuple 
+set 
+dict
+
+# Classes: custom data types 
+class
+
+# Specialized Data Types: modules, extensions
+
+# None: absence of value
+None
---
+++ 2.Python_Basics_1/2_data_types.py
@@ -0,0 +1,16 @@
+# Fundamental Data types 
+# Numbers
+
+print(2+4, type(2+4))
+print(2-4, type(2-4))
+print(2*4, type(2*4))
+print(2/4, type(2/4))
+
+# Power
+print(2 ** 4)
+
+# Double devide: it returns a integer rounded down
+print(2 // 4)
+
+# Modulo: It gets the reminder of this devision
+print(2 % 4)
