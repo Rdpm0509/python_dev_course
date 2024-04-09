91dcdd27d1faab8e5daa70076e7a673d462b7cb8
Rdpm0509 <rdpm0509@gmail.com>
2024-04-10 00:15:24 +0200

    Introducing strings

---
+++ 2.Python_Basics_1/5_string.py
@@ -0,0 +1,23 @@
+print(type('hi there'))
+username = 'supercoder'
+password = 'supersecret'
+long_string = '''
+WOW
+O O
+---
+'''
+print(long_string)
+
+first_name = 'Renan'
+last_name = 'Maciel'
+full_name = first_name + ' ' + last_name
+print(full_name)
+
+# String concatenation 
+# Adding strings together
+
+print('hello' + 'Renan')
+
+# Escape sequence 
+weather = "\t It\'s \"Kind of\" sunny \n hope you have a good day!"
+print(weather)
