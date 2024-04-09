8249507053a3e28e1868b2116afd8e5dc6ab3d06
Rdpm0509 <rdpm0509@gmail.com>
2024-04-10 00:01:56 +0200

    Variables and best practices

--- 2.Python_Basics_1/3_math_functions.py
+++ 2.Python_Basics_1/3_math_functions.py
@@ -6,3 +6,4 @@ print(abs(-20))
 # Binary number
 print(bin(5))
 print(int('0b101', 2))
+
---
+++ 2.Python_Basics_1/4_variables.py
@@ -0,0 +1,26 @@
+# Best practices when defining variables 
+
+# snake_case
+# start with lowercase or underscore
+# letters, numbers, underscore
+# case sensitive
+# dont overwrite keywords
+
+#  We are gonna come back to this later ... 
+
+# _  : designed for private variable
+# __ : dunder variables
+
+# Expressions VS Statments
+# Expression: Piece of code thaht produces a value
+# A Statment: An entire line of code
+iq = 1000
+user_iq = iq / 5
+
+
+# Augmented assignmennt operator 
+some_value = 5 
+some_value = some_value + 2 
+some_value += 2 
+print(some_value)
+
