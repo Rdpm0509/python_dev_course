46a377082667a499dfd0304fcc25d8c332ad002e
Rdpm0509 <rdpm0509@gmail.com>
2024-04-10 23:45:05 +0200

    bool

---
+++ 2.Python_Basics_1/7_booleans.py
@@ -0,0 +1,9 @@
+# Booleans, can either be True or False. 
+
+name = 'Renan'
+is_cool = False
+is_cool = True
+
+# Idea of boolean to denote logic
+print(bool(0)) # -> False
+print(bool(1)) # -> True
